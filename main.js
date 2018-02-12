d67a3d7cc5e6fa0a85e9d5e6e2e4f950d8d3088f
Vasilii Bodnariuk <i@vasilesk.ru>
2018-02-12 15:55:11 +0300

    add player released

--- main.js
+++ main.js
@@ -57,3 +57,37 @@ function imitGetPlayers() {
   rootElem.innerHTML = '';
   data.forEach(function(player){putPlayer(player, rootElem)});
 }
+
+function addPlayer() {
+  var xhr = new XMLHttpRequest();
+  var url = "/api/method/players.add";
+  var rating = parseFloat(document.getElementById('add-rating').value)
+  if (rating > 1.0) {
+    rating = 0.0;
+  }
+  var data = {
+    v: 1.0,
+    name: document.getElementById('add-name').value,
+    rating: rating,
+  };
+  var dataToSend = JSON.stringify(data);
+
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var data = JSON.parse(xhr.responseText);
+      if (data.status == 'error') {
+        document.getElementById("err-message").innerHTML = "Error: " + data.message;
+        console.log( "Error " + data.error);
+      } else {
+        data = data.data;
+        console.log(data);
+        document.getElementById("err-message").innerHTML = '';
+      }
+    } else {
+      document.getElementById("err-message").innerHTML = "Ошибка при добавлении игрока";
+    }
+  };
+  xhr.send(dataToSend);
+}
